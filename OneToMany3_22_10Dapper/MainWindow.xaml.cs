2988fa5340fb75f10acdc5ab47d1083f1471265e
Elvin Jamalzade <camalzadeelvin@itstep.edu.az>
2024-02-13 11:19:50 +0400

    Add project files.

---
+++ OneToMany3_22_10Dapper/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Dapper;
+using OneToMany3_22_10Dapper.Entities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OneToMany3_22_10Dapper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var conn = ConfigurationManager.ConnectionStrings["MyConn"].ConnectionString;
+            using (var connection = new SqlConnection(conn))
+            {
+                var sql = @"
+                        SELECT S.StudentId,S.Firstname,S.Age,S.GroupId,
+                        G.GroupId,G.Title
+                        FROM Students AS S
+                        INNER JOIN Groups AS G
+                        ON S.GroupId=G.GroupId
+            ";
+
+                var students = connection.Query<Student, Entities.Group, Student>(sql,
+                    (s, g) =>
+                    {
+                        s.Group = g;
+                        return s;
+                    }, splitOn: nameof(Student.GroupId));
+
+                studentsGrid.ItemsSource = students;
+            }
+
+
+            using (var connection=new SqlConnection(conn))
+            {
+
+                var sql = @"SELECT G.GroupId,G.Title,S.StudentId,S.Firstname,
+                            S.Age
+                            FROM Groups AS G
+                            INNER JOIN Students AS S
+                            ON G.GroupId=S.GroupId";
+
+                var groups = connection.Query<Entities.Group, Student, Entities.Group>(sql,
+                    (group, student) =>
+                    {
+                        group.Students.Add(student);
+                        student.GroupId = group.GroupId;
+                        student.Group = group;
+                        return group;
+                    }, splitOn: nameof(Student.GroupId)).ToList();
+                groupsGrid.ItemsSource = groups;
+            }
+
+
+        }
+    }
+}
---
+++ OneToMany3_22_10Dapper/SQLQuery1.sql
@@ -0,0 +1,35 @@
+﻿﻿	create database OneToManyDb
+	GO
+	use OneToManyDb
+	GO
+	
+	create table Groups(
+	GroupId int primary key identity(1,1) not null,
+	[Title] nvarchar(30) not null
+	)
+	
+	GO
+	
+	
+	
+	create table Students(
+	StudentId int primary key identity(1,1) not null,
+	[Firstname] nvarchar(30) not null,
+	[Age] int not null,
+	GroupId int foreign key references Groups(GroupId)
+	)
+	
+	GO
+	insert into Groups([Title])
+	values('2201_az'),('12012_az')
+	GO
+	
+	insert into Students([Firstname],[Age],[GroupId])
+	values('Kamran',23,1),('Lale',25,1),
+	('Murad',18,2),('Mehrac',28,2)
+	
+	GO
+	
+	select * from Students
+	inner join Groups
+	on Groups.GroupId=Students.GroupId
